6d85a03f2cb22f4075eae1ab72b40e0b7873ac8d
Boyan Atanasov <bojan.atanasov2002@gmail.com>
2023-05-06 22:26:54 +0300

    Created bonuses functionality in the Manager class, created tests for it, made about 80% of the functionality of the printingfactory class - need to finish that and write tests.

--- src/main/java/PrintingFactoryDriver/Main.java
+++ src/main/java/PrintingFactoryDriver/Main.java
@@ -8,6 +8,11 @@ import PrintingFactoryMachinery.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        PrintingFactory printingFactory1 = new PrintingFactory("printingFactory1", 1000);
+        printingFactory1.setIncome(1200);
+        Manager manager1 = new Manager("manager1", 1000, printingFactory1);
+        printingFactory1.addEmployee(manager1);
 
+        System.out.println(manager1.getBaseSalary());
     }
 }
